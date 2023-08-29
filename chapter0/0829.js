244658ca7c48f477231cea58a5b204b428039cf1
kimkmin06 <kimkmin06@gmail.com>
2023-08-29 16:51:21 +0900

    0829. 계산 예제

---
+++ chapter0/0829.js
@@ -0,0 +1,30 @@
+var num = 0;
+if (num == 0) {
+  let num = 100;
+} else {
+  let num = 200;
+}
+console.log(num);
+
+// let add = () => {
+//   return num1 - num2;
+// }
+// let sub = () => {
+//   return num1 - num2;
+// }
+// let mul = (num1,num2) => {
+//   return num1 * num2;
+// }
+// let div = (num1,num2) => {
+//   if (num2 == 0) return 0;
+//   return num1 / num2;
+// }
+
+// let result = add(10, 20);
+// console.log('result : ', result);
+// result = sub(10, 20);
+// console.log('result : ', result);
+// result = mul(10, 20);
+// console.log('result : ', result);
+// result = div(10, 20);
+// console.log('result : ', result);
