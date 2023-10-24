0ea0f8533331693203ffc4bdb92cce38542e0744
kimkmin06 <kimkmin06@gmail.com>
2023-10-24 15:21:45 +0900

    1024

--- learn-sequelize/app.js
+++ learn-sequelize/app.js
@@ -3,9 +3,18 @@ const path = require('path');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 
+const {sequelize} =require('./models');
+
 const app = express();
 app.set('port', process.env.PORT || 3001);
 
+sequelize.sync({force:false})
+.then(()=>{
+  console.log("데이터베이스 연결 성공...");
+})
+.catch((err)=>{
+  console.error(err);
+});
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ learn-sequelize/models/vue.js
@@ -0,0 +1,9 @@
+const Sequelize = require("sequelize");
+class User extends Sequelize.Model {
+  static init(sequelize) {
+
+  }
+  static associate(db) {
+
+  }
+};
